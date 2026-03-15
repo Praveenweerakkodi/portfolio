117b161c59078a239f590c24fe0556a94ca9bce3
Praveen Weerakkodi <praveenweerakkodi@gmail.com>
2026-03-15 22:01:43 +0530

    first commit

---
+++ src/utils/slug.js
@@ -0,0 +1,9 @@
+export const toSlug = (title) => {
+  if (!title || typeof title !== 'string') {
+    return '';
+  }
+  return title
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9-]/g, "");
+};
